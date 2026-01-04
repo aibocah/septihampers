9613cdd30683ae48cca55edc4e54bacf18e367fa
aibocah <aibocah86@gmail.com>
2026-01-04 15:24:06 +0700

    Update order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,15 +1,8 @@
 function kirimWA(){
-const nama=document.getElementById('nama').value;
-const hp=document.getElementById('hp').value;
-const alamat=document.getElementById('alamat').value;
-const pesanan=document.getElementById('pesanan').value;
-const catatan=document.getElementById('catatan').value;
-if(!nama||!hp||!alamat||!pesanan){alert('Lengkapi data');return;}
-let text=`Halo, saya ingin pesan hampers%0A
-Nama: ${nama}%0A
-HP: ${hp}%0A
-Alamat: ${alamat}%0A
-Pesanan: ${pesanan}%0A
-Catatan: ${catatan}`;
-window.location.href='https://wa.me/62895339847320?text='+text;
+  const data = [...document.querySelectorAll('input,textarea')]
+    .map(e => `${e.placeholder}: ${e.value}`)
+    .join('%0A');
+
+  window.location.href =
+  'https://wa.me/62895339847320?text=Halo%0A' + data;
 }
